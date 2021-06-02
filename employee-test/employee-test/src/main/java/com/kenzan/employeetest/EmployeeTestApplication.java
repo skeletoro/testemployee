6bbf87b9335204c0c5ddd440a301cb3301d355da
Your Name <skeletoro@github.com>
2021-06-02 10:04:40 -0700

    zh uploading springboot maven demo through bash

---
+++ employee-test/employee-test/src/main/java/com/kenzan/employeetest/EmployeeTestApplication.java
@@ -0,0 +1,13 @@
+package com.kenzan.employeetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTestApplication.class, args);
+	}
+
+}
---
+++ employee-test/employee-test/src/test/java/com/kenzan/employeetest/EmployeeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenzan.employeetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
